676ef2d767cbf555dca222e2d5a4253f78d3e8c5
Juvenal A. Silva Jr <juvenal.silva.jr@gmail.com>
2009-11-27 16:39:06 -0200

    - First test build to perform...

--- build.sh
+++ build.sh
@@ -11,9 +11,11 @@
 #
 
 # Define some destinations
-BUILDPKG=$(pwd)
-INSTALLPKG="/usr/local"
-CONTAINERPKG=${BUILDPKG}/"Disk Image"
+WORKDIR=$(pwd)
+INSTALL="/usr/local"
+BUILDPKG="${WORKDIR}/git_build"
+TINSTPKG="${WORKDIR}/git_install"
+CONTAINERPKG="${WORKDIR}/Disk Image"
 
 # ===========================================================================
 # Hopefully you won't need to change anything below this point.  The
@@ -21,7 +23,7 @@ CONTAINERPKG=${BUILDPKG}/"Disk Image"
 # ===========================================================================
 
 # Conditional define
-if [ "`uname`" == "Darwin" ]; then
+if [ "$(uname)" == "Darwin" ]; then
 	sed_regexp="-E"
 else
 	sed_regexp="-r"
@@ -35,21 +37,19 @@ export IMAGE_FILENAME="git-${GIT_VERSION}-leopard.dmg"
 
 # Internal functions to perform the complete build
 function build_universal_binary {
-	# Prepare some internal variables
-	local PREFIX=${1}
-	local DESTDIR=${2}
 	# Inform and start the build process
 	echo "Building GIT $GIT_VERSION"
-	# Prepare the build area
-	[ ! -d git_build ] && \
-		mkdir -p git_build
+	# Go to the build area
+	pushd ${WORKDIR}
+	[ ! -d ${BUILDPKG} ] && \
+		mkdir -p ${BUILDPKG}
 	# Go to the build area and refresh it
-	pushd git_build
+	pushd ${BUILDPKG}
 	[ ! -f git-${GIT_VERSION}.tar.bz2 ] && \
 		curl -O http://kernel.org/pub/software/scm/git/git-${GIT_VERSION}.tar.bz2
 	[ -d git-${GIT_VERSION} ] && \
 		rm -rf git-${GIT_VERSION}
-	tar jxvf git-${GIT_VERSION}.tar.bz2
+	tar xjvf git-${GIT_VERSION}.tar.bz2
 	# Enter and perform the build and install
 	pushd git-${GIT_VERSION}
 	# If you're on PPC, you may need to uncomment this line: 
@@ -59,28 +59,31 @@ function build_universal_binary {
 	echo "NO_CROSS_DIRECTORY_HARDLINKS=1" >> Makefile_tmp
 	cat Makefile >> Makefile_tmp
 	mv Makefile_tmp Makefile
-	# Build fat binaries with ppc and i386 code for Leopard (10.5)
+	# Build fat binaries with ppc and i386 code for Leopard (10.5) 32 and 64 bits
 	make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
 		 LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
-		 prefix=${PREFIX} all
+		 prefix=${INSTALL} all
 	make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
 		 LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
-		 prefix=${PREFIX} strip
+		 prefix=${INSTALL} strip
 	make CFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
 		 LDFLAGS="-isysroot /Developer/SDKs/MacOSX10.5.sdk -arch ppc -arch i386 -arch ppc64 -arch x86_64" \
-		 prefix=${PREFIX} DESTDIR=${DESTDIR} install
+		 prefix=${INSTALL} DESTDIR=${TINSTPKG} install
 	# Add the contrib completion file
-	mkdir -p ${DESTDIR}/usr/local/share/git/contrib/completion
-	cp contrib/completion/git-completion.bash ${DESTDIR}/usr/local/share/git/contrib/completion/
+	mkdir -p ${TINSTPKG}/usr/local/share/git/contrib/completion
+	cp contrib/completion/git-completion.bash ${TINSTPKG}/usr/local/share/git/contrib/completion/
 	popd
 	# Collect and install the man pages
 	[ ! -f git-manpages-${GIT_VERSION}.tar.bz2 ] && \
 		curl -O http://www.kernel.org/pub/software/scm/git/git-manpages-${GIT_VERSION}.tar.bz2
-	mkdir -p ${DESTDIR}/usr/local/share/man
-	tar xjvo -C ${DESTDIR}/usr/local/share/man -f git-manpages-${GIT_VERSION}.tar.bz2
+	[ -d ${TINSTPKG}/usr/local/share/man ] && \
+		rm -rf ${TINSTPKG}/usr/local/share/man
+	mkdir -p ${TINSTPKG}/usr/local/share/man
+	tar xjvo -C ${TINSTPKG}/usr/local/share/man -f git-manpages-${GIT_VERSION}.tar.bz2
+	popd
 	popd
 	# add .DS_Store to default ignore for new repositories
-	echo ".DS_Store" >> "${DESTDIR}/usr/local/share/git-core/templates/info/exclude"
+	echo ".DS_Store" >> "${TINSTPKG}/usr/local/share/git-core/templates/info/exclude"
 	# Inform the build completion
 	echo "Finished build GIT ${GIT_VERSION}"
 	# End function
@@ -88,7 +91,8 @@ function build_universal_binary {
 
 function build_package_container {
 	# Define some local variables
-	local CONTAINER=${1}
+	local DESTDIR=${1}
+	local CONTAINER=${2}
 	# Prepare the stage and remove old installers
 	[ ! -d ${CONTAINER} ] && \
 		mkdir -p ${CONTAINER}
@@ -96,26 +100,27 @@ function build_package_container {
 		rm -rf ${CONTAINER}/*.pkg
 	# Categorize the app
 	# First of all prepare the application folder structure
-	[ ! -d gitCmdLn-${GIT_VERSION} ] && \
-		mkdir gitCmdLn-${GIT_VERSION}
-	[ ! -d gitCmdLn-${GIT_VERSION}/component ] && \
-		mkdir gitCmdLn-${GIT_VERSION}/component
-	[ ! -d gitCmdLn-${GIT_VERSION}/extras ] && \
-		mkdir -p gitCmdLn-${GIT_VERSION}/extras
+	rm -rf gitApp-${GIT_VERSION}
+	rm -rf gitCmdLn-${GIT_VERSION}
+	mkdir -p gitApp-${GIT_VERSION}/component
+	mkdir -p gitCmdLn-${GIT_VERSION}/component
+	mkdir -p gitCmdLn-${GIT_VERSION}/extras
+	# Fill the components and extras for the gitApp part
+	[ -d gitApp-${GIT_VERSION}/extras
 	# Now fill it with the needed files
-	[ -d gitCmdLn-${GIT_VERSION}/component/* ] && \
-		rm -rf gitCmdLn-${GIT_VERSION}/component/* && \
-		cp -r usr/local/* gitCmdLn-${GIT_VERSION}/component
-	[ -f gitCmdLn-${GIT_VERSION}/extras/* ] && \
-		rm -rf gitCmdLn-${GIT_VERSION}/extras/* && \
+	cp -r ${DESTDIR}/usr/local/* gitCmdLn-${GIT_VERSION}/component
+	cat 
+	# Fill the components and extras for the gitCmdLn part
+	[ -d gitCmdLn-${GIT_VERSION}/component
 	
 	# End function
 }
 
 # Run the binary build script
-build_universal_binary ${INSTALLPKG} ${BUILDPKG}
+build_universal_binary
+exit 0
 # Prepare the package container
-build_package_container ${CONTAINERPKG}
+build_package_container ${BUILDPKG} ${CONTAINERPKG}
 
 
 
